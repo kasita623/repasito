876c3a20ecf29119377a82d4d25ac6a2b295f440
kasita623 <kasiajura@onet.pl>
2022-01-18 22:03:16 +0100

    function displayDate and displayTime

--- src/app.js
+++ src/app.js
@@ -1,3 +1,52 @@
+function formatTime(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let week = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let months = [
+    "December",
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+  ];
+  let weekDay = week[date.getDay()];
+  let month = months[date.getMonth()];
+  let day = date.getDate();
+  if (day < 10) {
+    day = `0${day}`;
+  }
+
+  return `${weekDay}, ${month} ${day}`;
+}
+
 function displayWeather(response) {
   document.querySelector("#temp").innerHTML = Math.round(
     response.data.main.temp
@@ -17,7 +66,13 @@ function displayWeather(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
-  console.log(response.data);
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
+  document.querySelector("#time").innerHTML = formatTime(
+    response.data.dt * 1000
+  );
+  console.log(response.data.dt);
 }
 
 let apiKey = `940cab7f2dffe0039b455473a663a1f7`;
